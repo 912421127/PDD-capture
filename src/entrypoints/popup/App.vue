2db3a3e404c2252f6ae84b818c624c22d5a5221e
912421127 <912421127@qq.com>
2026-07-03 14:56:11 +0800

    chore: scaffold wxt vue extension

---
+++ src/entrypoints/popup/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="popup-page">
+    <a-typography>
+      <a-typography-title :level="4">PDD 店铺数据采集</a-typography-title>
+      <a-typography-paragraph class="status-text">
+        当前版本仅完成插件框架
+      </a-typography-paragraph>
+    </a-typography>
+
+    <a-button type="primary" block>开始配置</a-button>
+
+    <a-alert
+      class="tip-box"
+      type="info"
+      show-icon
+      message="后续会逐步加入登录检测、订单采集、商品采集"
+    />
+  </main>
+</template>
